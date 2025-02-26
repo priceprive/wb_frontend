11ddf49c5eaef42bd6915649670fb4a16c6720cb
ritty025 <ritikapradhan025@gmail.com>
2025-02-26 17:10:11 +0530

    added changes

--- website/auth.py
+++ website/auth.py
@@ -19,57 +19,50 @@ auth = Blueprint('auth',__name__)
 #     workbook.save(EXCEL_FILE)
 
 
-@auth.route('/company',methods=['GET','POST'])
-def company():
+@auth.route('/contact',methods=['GET','POST'])
+def contact():
     if request.method == 'POST':
+        name = request.form.get('name')
         email = request.form.get('email')
-        phone = request.form.get('phone')
+        message = request.form.get('message')
         
-        if len(phone)<10:
-            flash('Phone no must be atleast 10 digit')
-        else:
-            # Store the submitted data in the database
-            new_user = User(email=email, phone=phone)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Successfully submitted')
+        # if len(phone)<10:
+        #     flash('Phone no must be atleast 10 digit')
+        # else:
+            
+        #     new_user = User(email=email, phone=phone)
+        #     db.session.add(new_user)
+        #     db.session.commit()
+        #     flash('Successfully submitted')
 
-             # Append data to the Excel file
-            # workbook = openpyxl.load_workbook(EXCEL_FILE)
-            # sheet = workbook.active
-            # sheet.append([email, phone])  
-            # workbook.save(EXCEL_FILE)
+             
 
-            return redirect(url_for('auth.success'))
-    return render_template('company.html')
+        #     return redirect(url_for('auth.success'))
+    return render_template('contact.html')
 
 
 @auth.route('/success')
 def success():
     return "<h1>Submission Successful!</h1>"
 
-# @auth.route('/')
-# def index():
-#     users = User.query.all()
-#     users_data = [{'id': user.id,'email': user.email, 'phone': user.
-#     phone} for user in users]
-#     print(users_data)
-#     return render_template("index.html", users=users)
 
 
 @auth.route('/')
-def home():
-    return render_template('home.html')
+def competitor():
+    return render_template('competitor.html')
 
 
 @auth.route('/pricing')
 def pricing():
     return render_template('pricing.html')
 
-@auth.route('/product')
-def product():
-    return render_template('Products.html')
+@auth.route('/report')
+def report():
+    return render_template('report.html')
 
+@auth.route('/services')
+def services():
+    return render_template('services.html')
 
 
 # if __name__ == '__main__':
